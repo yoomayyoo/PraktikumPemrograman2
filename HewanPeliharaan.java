e66fb644472bd8bed8ca34dd8f05997bc30f2520
yoomayyoo <145313546+yoomayyoo@users.noreply.github.com>
2024-12-07 21:15:42 +0800

    Add files via upload

--- HewanPeliharaan.java
+++ HewanPeliharaan.java
@@ -1,17 +1,16 @@
-package soal2;
+package soal1;
 
 public class HewanPeliharaan {
 	private String nama;
 	private String ras;
 	
-	public HewanPeliharaan(String n, String r) {
-		this.nama = n;
+	public HewanPeliharaan(String r, String n) {
 		this.ras = r;
+		this.nama = n;
 	}
 	public void display() {
 		System.out.println("Detail Hewan Peliharaan:");
 		System.out.println("Nama hewan peliharaanku adalah: " + this.nama);
-		System.out.println("Dengan ras: "+ this.ras);
+		System.out.println("Dengan ras: " + this.ras);
 	}
-
 }

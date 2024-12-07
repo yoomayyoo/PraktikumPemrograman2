5667c1349afba86c5d307a21e1bb57dfb6e64098
yoomayyoo <145313546+yoomayyoo@users.noreply.github.com>
2024-12-07 21:44:08 +0800

    Add files via upload

---
+++ DetailHewan.java
@@ -0,0 +1,12 @@
+package soal1;
+
+public class DetailHewan {
+
+	public static void main(String[] args) {
+		HewanPeliharaan hewan = new HewanPeliharaan("Kucing Anggora", "Leo");
+		
+		hewan.display();
+
+	}
+
+}
